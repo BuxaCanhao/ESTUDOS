aadf560bc88c272ca0c40c39e245c34edd42ac2f
Bubu <93006203+BuxaCanhao@users.noreply.github.com>
2025-10-09 18:12:30 -0300

    alt-1812-091025

--- JavaScript/exercicios/ex-02/src/js/script.js
+++ JavaScript/exercicios/ex-02/src/js/script.js
@@ -1,2 +1,19 @@
+function resetar(){
+    var  fano = document.getElementById('txtano')
+    if(fano.value != 2000){
+        fano.value = '2000'
+    }
+}
+
 function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('#res')
+
+    if (fano.value.length ==0 || Number(fano.value)>ano || Number(fano.value)<1870){
+        window.alert('Verifique os dados e tente novamente')
+    }else{
+        
+    }
 }
