cafc5d5ff00461815534e2c6fd5cba8c193aa908
Bubu <93006203+BuxaCanhao@users.noreply.github.com>
2025-11-21 16:47:03 -0300

    alti-final-211125

--- JavaScript/exercicios/ex-04/src/js/javascript.js
+++ JavaScript/exercicios/ex-04/src/js/javascript.js
@@ -1,13 +1,19 @@
 function btinicio(){
+    //valores de entrada
     const valInicio = document.querySelector('#valinicio')
-    const valorEnt = +valInicio.value //O mais na frente passa para numero
+    const valPulo = document.querySelector('#valpulo')
+    const valSalto = +valPulo.value//O + na frente é para converter em número
+    const valorEnt = +valInicio.value//O + na frente é para converter em número
+    
+    //valores para ferrificação
     const result = document.querySelector('#bxmsg')
     const valInput = document.querySelector('.inputval')
     
+    //reseta o style e o html
     valInput.style =''
     result.innerHTML =''
     
-    //verificação de validade de entrada
+    //verificação de validade do 1º valor
     if(valorEnt>10 || valorEnt<1){
         //gera a mensagem de alerta
         let divMsgAler = document.createElement('div')
@@ -19,12 +25,30 @@ function btinicio(){
         valInicio.style.fontWeight = 'bold'
         
         if(valorEnt == ''){
-                divMsgAler.innerHTML = `Só pode inserir número de 1 a 10`
+                divMsgAler.innerHTML = `No 1º valor só pode inserir número de 1 a 10`
             }else if(valorEnt>10){
-                divMsgAler.innerHTML = `Esse valor de ${valorEnt}, está acima de 10<br>Só pode inserir número de 1 a 10`
+                divMsgAler.innerHTML = `O 1º valor de ${valorEnt}, está acima de 10<br>Só pode inserir número de 1 a 10`
             }else if(valorEnt<1){
-                divMsgAler.innerHTML = `Esse valor de ${valorEnt}, está abaixo de 1<br>Só pode inserir número de 1 a 10`
+                divMsgAler.innerHTML = `O 1º valor de ${valorEnt}, está abaixo de 1<br>Só pode inserir número de 1 a 10`
             }
+        }
+    //verificação de validade do 2º valor
+    if(valSalto>10 || valSalto<1){
+        //gera a mensagem de alerta
+        let divMsgAler = document.createElement('div')
+        divMsgAler.id = 'msgAlert'
+        result.append(divMsgAler)
 
+        //troca a cor do txt do input
+        valPulo.style.color = 'red'
+        valPulo.style.fontWeight = 'bold'
+        
+        if(valSalto == ''){
+                divMsgAler.innerHTML = `No 2º valor só pode inserir número de 1 a 10`
+            }else if(valSalto>10){
+                divMsgAler.innerHTML = `O 2º valor de ${valSalto}, está acima de 10<br>Só pode inserir número de 1 a 10`
+            }else if(valSalto<1){
+                divMsgAler.innerHTML = `O 2º valor de ${valorEnt}, está abaixo de 1<br>Só pode inserir número de 1 a 10`
+            }
         }
 }
