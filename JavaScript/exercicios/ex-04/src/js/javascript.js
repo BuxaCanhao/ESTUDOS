7947dcb746af90656f3a65c06bc15b35474944de
Bubu <93006203+BuxaCanhao@users.noreply.github.com>
2025-11-21 10:31:35 -0300

    alt-211125-1031

--- JavaScript/exercicios/ex-04/src/js/javascript.js
+++ JavaScript/exercicios/ex-04/src/js/javascript.js
@@ -1,7 +1,17 @@
 function btinicio(){
     const valInicio = document.querySelector('#valinicio')
-    const result = document.querySelector('#result')
+    const result = document.querySelector('main')
+    const valInput = document.querySelector('.inputval')
     
-    result.innerHTML = (`o valor é ${valInicio.value}`)
-    result.style.display = 'block'
+    valInput.style = ''
+    
+    if(valInicio.value>10 || valInicio.value<1){
+        //troca a cor do txt do input
+        valInicio.style.color = 'red'
+        valInicio.style.fontWeight = 'bold'
+
+        //gera a mensagem de alerta
+    }else{
+        //gera o resultado
+    }
 }
