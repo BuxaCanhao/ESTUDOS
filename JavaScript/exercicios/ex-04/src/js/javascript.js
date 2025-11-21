4cf5c8894ddab003e242bf20f63fd0723721dd62
Bubu <93006203+BuxaCanhao@users.noreply.github.com>
2025-11-21 16:01:07 -0300

    alt-1601

--- JavaScript/exercicios/ex-04/src/js/javascript.js
+++ JavaScript/exercicios/ex-04/src/js/javascript.js
@@ -1,17 +1,29 @@
 function btinicio(){
     const valInicio = document.querySelector('#valinicio')
-    const result = document.querySelector('main')
+    const result = document.querySelector('#bxmsg')
     const valInput = document.querySelector('.inputval')
     
-    valInput.style = ''
+    valInput.style =''
+    result.innerHTML =''
     
+    //verificação de validade de entrada
     if(valInicio.value>10 || valInicio.value<1){
+        //gera a mensagem de alerta
+        let divMsgAler = document.createElement('div')
+        divMsgAler.id = 'msgAlert'
+        result.append(divMsgAler)
+
         //troca a cor do txt do input
         valInicio.style.color = 'red'
         valInicio.style.fontWeight = 'bold'
-
-        //gera a mensagem de alerta
-    }else{
-        //gera o resultado
-    }
+        
+        if(valInicio.value == ''){
+                divMsgAler.innerHTML = `Só pode inserir número de 1 a 10`
+            }else if(valInicio.value>10){
+                divMsgAler.innerHTML = `Esse valor de ${valInicio.value}, está acima de 10<br>Só pode inserir número de 1 a 10`
+            }else if(valInicio.value<1){
+                divMsgAler.innerHTML = `Esse valor de ${valInicio.value}, está abaixo de 1<br>Só pode inserir número de 1 a 10`
+            }
+            
+        }
 }
