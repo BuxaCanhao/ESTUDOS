59a88905d9cd07fe1900b38778b254c5ca5ec5e6
Bubu <93006203+BuxaCanhao@users.noreply.github.com>
2025-12-04 15:34:06 -0300

    .

--- React/aula-02_componentes/src/components/Component.js
+++ React/aula-02_componentes/src/components/Component.js
@@ -1,7 +1,10 @@
+import './Component.css';
+
 const Component = () => {
     return (
         <div>
-            <h1>teste</h1>
+            <h1 className='txt' id='exemplo'>teste</h1>
+            <p className='txt'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi architecto ipsum tempora, porro voluptatem deserunt ex quos optio atque nisi ad sunt et alias facilis vel. Accusantium ducimus hic corporis.</p>
         </div>
     )
 }
---
+++ React/aula-02_componentes/src/components/NovoComponent.js
@@ -0,0 +1,9 @@
+const NovoComponent = () =>{
+    return(
+        <div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse animi necessitatibus totam accusamus quasi, nobis unde autem officiis id cupiditate cumque ratione modi architecto reiciendis dolorum? Porro beatae ipsum tenetur.</p>
+        </div>
+    )
+}
+
+export default NovoComponent
