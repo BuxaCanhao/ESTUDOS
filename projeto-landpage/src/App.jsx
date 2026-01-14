11453b939054c1dd79785f15ee09a714847e044c
Bubu <93006203+BuxaCanhao@users.noreply.github.com>
2026-01-14 17:02:41 -0300

    alt de hj

--- projeto-landpage/src/App.jsx
+++ projeto-landpage/src/App.jsx
@@ -1,15 +1,19 @@
 import AddTask from "./conponents/AddTask";
 import Task from "./conponents/Task";
 
-function App() {
+import "./App.css";
 
+function App() {
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p6">
-      <div className="w-[500px]">
-        <h1  className="text-3xl text-slate-100 font-bold text-center">
+    <div className="w-screen h-screen bg-sky-200 dark:bg-amber-900 flex justify-center p6">
+      <div className="w-[500px] h-dvh bg-amber-50 dark:bg-[#acc7c7] rounded-t-[500px] outline-10 outline-offset-4">
+        <h1 className="text-4xl text-blue-300 font-bold text-center text-shadow-2xs">
           Gerenciador de Tarefas
         </h1>
-        <Task/>
+        <p className="text-zinc-500 font-extrabold text-3xl">
+          teste
+        </p>
+        <Task />
         <AddTask />
       </div>
     </div>
